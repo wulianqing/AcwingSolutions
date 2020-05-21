b64e98ab96054cf001e8eb041fb6d6d0562bbfe9
Aaronwilliamng <Aaronwilliamng@gmail.com>
2020-05-21 11:32:17 +0800

    update

---
+++ tecent2020__worst_logn_BinarySearchLast.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+int main(){
+    int num_length;
+    std::cin >> num_length;
+    int* nums = new int[num_length];
+    for (int i = 0; i < num_length;i++){
+        std::cin >> nums[i];
+    }
+    int target;
+    std::cin >> target;
+    int left = 0, right = num_length - 1;
+    int mid;
+    while(left<right){
+        //+ 1:Otherwise can not pick the last number when numbers are equal
+        mid = left + (right - left) / 2 + 1; 
+        //target is in right part
+        if(nums[mid] > target){
+            right = mid - 1;
+        }
+        //in left part
+        else{
+            left = mid;
+        }
+    }
+    //left == right,maybe not found
+    if(nums[left]==target)
+        std::cout << mid << std::endl;
+    else
+        std::cout << "not fount\n";
+    return 0;
+}
