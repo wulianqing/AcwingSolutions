6d6d7bd59e62eb71cd8f320735ec84d56dfb8f1c
Aaronwilliamng <Aaronwilliamng@gmail.com>
2020-06-01 11:16:42 +0800

    update

--- offer_47_mostValuableGiftPath.cpp
+++ offer_47_mostValuableGiftPath.cpp
@@ -27,34 +27,17 @@ public:
     int maxValue_2(vector<vector<int>>& grid){
         int row = grid.size();
         int col = grid[0].size();
-        if(row == 0)
+        if(row == 0 || col == 0)
             return 0;
-        vector<vector<int>> dp_table = vector<vector<int>>(row, vector<int>(col));
-        dp_table[0][0] = grid[0][0];
-        //exception
-        if(row == 1 && col && 1)
-            return dp_table[0][0];
-        if(row == 1){
-            int max_sum = 0;
-            for (int i = 0; i < col;i++)
-                max_sum += grid[0][i];
-            return max_sum;
-        }
-        if(col == 1){
-            int max_sum = 0;
-            for (int i = 0; i < row;i++)
-                max_sum += grid[i][0];
-            return max_sum;
-        }
-        //init dp table
-        for (int i = 1; i < col;i++)
-            dp_table[0][i] = dp_table[0][i - 1] + grid[0][i];
-        for (int i = 1; i < row;i++)
-            dp_table[i][0] = dp_table[i - 1][0] + grid[i][0];
+        vector<vector<int>> dp_table(row, vector<int>(col,0));
         //calculate dp table
-        for (int i = 1; i < row;i++){
-            for (int j = 1; j < col;j++)
-                dp_table[i][j] = max(dp_table[i - 1][j], dp_table[i][j - 1]) + grid[i][j];
+        for (int i = 0; i < row;i++){
+            for (int j = 0; j < col;j++){
+                if(i == 0 && col == 0)dp_table[0][0] = grid[0][0];
+                else if(i == 0)dp_table[i][j] = dp_table[i][j-1] + grid[i][j];
+                else if(j == 0)dp_table[i][j] = dp_table[i-1][j] + grid[i][j];
+                else dp_table[i][j] = max(dp_table[i - 1][j], dp_table[i][j - 1]) + grid[i][j];
+            }
         }
         return dp_table[row - 1][col - 1];
     }
