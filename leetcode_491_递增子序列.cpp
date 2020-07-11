e9c188f4fa391b4d044fe223c143b965b1cf4038
Aaronwilliamng <Aaronwilliamng@gmail.com>
2020-07-11 21:59:15 +0800

    update

--- leetcode_491_递增子序列.cpp
+++ leetcode_491_递增子序列.cpp
@@ -8,12 +8,13 @@ public:
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         vector<int> path;
         vector<vector<int>> ans;
-        unordered_map<vector<int>, bool> hm;
-        dfs(nums, path, 0, ans, hm);
+        unordered_map<string, bool> hm;
+        for (int i = 0; i < nums.size();i++)
+            dfs(nums, path, i, ans, hm);
         return ans;
     }
 
-    void dfs(vector<int>& nums,vector<int>& path,int cur_pos,vector<vector<int>>& ans,unordered_map<vector<int>, bool>& hm){
+    void dfs(vector<int>& nums,vector<int>& path,int cur_pos,vector<vector<int>>& ans,unordered_map<string, bool>& hm){
         //stop condition
         if(cur_pos >= nums.size())
             return;
@@ -21,15 +22,14 @@ public:
         if(nums[cur_pos] >= nums[path.size()-1])
             path.push_back(nums[cur_pos]);
         //if add path to ans
-        
-        /*
-        if(hm.find(path) == hm.end()){
-            hm.insert(make_pair(path, true));
-            ans.push_back(path);
+        string i_path;
+        for (int i = 0; i < path.size();i++)
+            i_path.push_back((char)path[i]);
+        if (i_path.size() >= 2 && hm.find(i_path) == hm.end())
+        {
+            hm.insert(make_pair(i_path, true));
+            ans.push_back(path);            
         }
-        */
-       
-        ans.push_back(path);
         //visit rest positions
         for (int i = cur_pos + 1; i < nums.size();i++)
             dfs(nums, path, i, ans, hm);
