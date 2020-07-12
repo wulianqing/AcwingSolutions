b7293d02a5fc9eecb8bbf918d84b0a66a430b9a2
Aaronwilliamng <Aaronwilliamng@gmail.com>
2020-07-12 23:13:00 +0800

    udpate

--- leetcode_4_困难_两个升序数组的中位数_或_第k位数.cpp
+++ leetcode_4_困难_两个升序数组的中位数_或_第k位数.cpp
@@ -12,7 +12,7 @@ public:
             return nums2[k - n - 1];
         int left = 0, right = n - 1;
         int mid;
-        while(left < right){
+        while(left <= right ){
             mid =  left + (right - left) / 2;
             if(nums1[mid - 1] > nums2[k - mid - 1]){
                 right = mid;
@@ -24,16 +24,25 @@ public:
             }
             return min(nums1[mid], nums2[k - mid - 1]);
         }
+        return 0;
     }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if((n1 + n2) % 2 == 1)
+            return findKthNumber(nums1, nums2, (n1 + n2) / 2 + 1);
+        else
+            return (double)(findKthNumber(nums1, nums2, (n1 + n2) / 2) + findKthNumber(nums1, nums2, (n1 + n2) / 2 + 1))/2;
     }
 };
 int main(){
     vector<int> nums1 = {1, 3, 7, 8};
-    vector<int> nums2 = {2, 6, 9, 10, 11};
-    int k = 4;
+    vector<int> nums2 = {2, 6, 9, 10, 11,12};
+    vector<int> nums3 = {1, 3};
+    vector<int> nums4 = {2};
+    int k = 6;
     Solution solu;
     cout << solu.findKthNumber(nums1, nums2, k) << endl;
+    cout << solu.findMedianSortedArrays(nums3, nums4) << endl;
     return 0;
 }
