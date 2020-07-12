b476e75e6d85d3cfb3ced6e0560c0093cea3a1a9
Aaronwilliamng <Aaronwilliamng@gmail.com>
2020-07-12 10:35:15 +0800

    update

--- leetcode_491_递增子序列.cpp
+++ leetcode_491_递增子序列.cpp
@@ -19,16 +19,18 @@ public:
         if(cur_pos >= nums.size())
             return;
         //if add cur_pos to visited
-        if(nums[cur_pos] >= nums[path.size()-1])
+        if(path.size() == 0 || (path.size() > 0 && nums[cur_pos] >= nums[path.size()-1]))
             path.push_back(nums[cur_pos]);
         //if add path to ans
-        string i_path;
-        for (int i = 0; i < path.size();i++)
+        string i_path = "";
+        if(path.size() >=2){
+            for (int i = 0; i < path.size();i++)
             i_path.push_back((char)path[i]);
-        if (i_path.size() >= 2 && hm.find(i_path) == hm.end())
-        {
-            hm.insert(make_pair(i_path, true));
-            ans.push_back(path);            
+            if (i_path.size() >= 2 && hm.find(i_path) == hm.end())
+            {
+                hm.insert(make_pair(i_path, true));
+                ans.push_back(path);            
+            }
         }
         //visit rest positions
         for (int i = cur_pos + 1; i < nums.size();i++)
@@ -38,7 +40,7 @@ public:
     }
 };
 int main(){
-    vector<int> nums = {4, 6, 7, 7};
+    vector<int> nums = {4, 6, 6, 7};
     Solution solu;
     vector<vector<int>> ans = solu.findSubsequences(nums);
     for (int i = 0; i < ans.size();i++){
