e5f76f6123c5b4d5566059cabc1b5b90d63246a0
Aaronwilliamng <Aaronwilliamng@gmail.com>
2020-07-16 17:19:08 +0800

    update

--- leetcode_322_零钱兑换_不同面额凑n元最小张数.cpp
+++ leetcode_322_零钱兑换_不同面额凑n元最小张数.cpp
@@ -7,16 +7,21 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<int> dp(amount + 1);
-        if(amount <= 0)
-            return -1;
+        if(amount == 0)
+            return 0;
         dp[0] = 0;
         for (int i = 1; i <= amount;i++){
             //dp[i] = max dp[i - coin[j]] + 1
             int min_cur = INT_MAX;
             for (int j = 0; j < n;j++)
-                if(i - coins[j] >= 0 && dp[i - coins[j]] < min_cur)
+                //这里保证dp 不等于 -1, -1表示无法凑出该钱数
+                if(i - coins[j] >= 0 && dp[i - coins[j]] >= 0 && dp[i - coins[j]] < min_cur)
                     min_cur = dp[i - coins[j]];
-            dp[i] = min_cur + 1;
+            //遍历可选选择都不可行
+            if(min_cur == INT_MAX)
+                dp[i] = -1;
+            else 
+                dp[i] = min_cur + 1;
         }
         return dp[amount];
     }
